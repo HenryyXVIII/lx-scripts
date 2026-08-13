4b1642b4d2844e41c030e3d585b05a03baddf773
HenryyXVIII <wille.heinrich@outlook.de>
2026-08-13 15:24:26 +0200

    Fix syntax error in basiscs.sh script

--- install/basics/basiscs.sh
+++ install/basics/basiscs.sh
@@ -5,9 +5,9 @@ sudo -n true
 test $? -eq 0 || {
     echo "you should have sudo privilege to run this script"
     exit 1
-    }
+}
 
-apt update && apt install -y fastfetch btop nano wget curl neofetch ufw 2>/dev/null
+apt update && apt install -y fastfetch btop nano wget curl neofetch ufw
 
 #INSTALLED=$(apt list --installed | grep -e 'fastfetch' -e 'neofetch' | awk '{print $1}' | awk -F/ '{print $1}' 2> /dev/null)
 WICHFETCH=$(dpkg -l | awk '$1 == "ii" && ($2 == "fastfetch" || $2 == "neofetch") {print $2}')
