67808ba3815b3fd90cd5f7d03dedac1ec16922e1
HenryyXVIII <wille.heinrich@outlook.de>
2026-08-13 15:27:34 +0200

    Modify package installation in basiscs.sh
    Updated package installation to include 'bat' and 'eza' while maintaining 'btop', 'nano', 'wget', 'curl', 'ufw', 'neofetch', and 'fastfetch'.

--- install/basics/basiscs.sh
+++ install/basics/basiscs.sh
@@ -7,7 +7,7 @@ test $? -eq 0 || {
     exit 1
 }
 
-apt update && apt install -y fastfetch btop nano wget curl neofetch ufw
+apt update && apt install -y bat eza btop nano wget curl ufw && apt install -y neofetch && apt install -y fastfetch
 
 #INSTALLED=$(apt list --installed | grep -e 'fastfetch' -e 'neofetch' | awk '{print $1}' | awk -F/ '{print $1}' 2> /dev/null)
 WICHFETCH=$(dpkg -l | awk '$1 == "ii" && ($2 == "fastfetch" || $2 == "neofetch") {print $2}')
