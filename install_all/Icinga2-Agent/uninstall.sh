8039cdc50f63f593cc285c78543e6a67ed43ceae
HenryyXVIII <wille.heinrich@outlook.de>
2026-09-17 11:39:20 +0200

    Enhance uninstall script with conditional directory removal
    Refactor the uninstall script to improve logging and remove directories conditionally.

--- install_all/Icinga2-Agent/uninstall.sh
+++ install_all/Icinga2-Agent/uninstall.sh
@@ -3,21 +3,28 @@
 ## Autor
 
 
+
+
+set -Eeuo pipefail
+
+
+
+VERSION="1.0.2"
+LOGFILE="/var/log/icinga2_uninstall.log"
 # Colors for output
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0;37m' # No Color
-
-set -Eeuo pipefail
+PACKAGES=("icinga2" "icinga2-bin" "icinga2-common" "monitoring-plugins")
+ICINGAFILES=("/etc/icinga2" "/var/lib/icinga2" "/var/log/icinga2")
 
 ############
 # LOG-FILE #
 ############
-LOGFILE="/var/log/icinga2_uninstall.log"
 
 log() {
-    echo -e "=> [$(date '+%F %T')] $*" | tee -a "$LOGFILE"
+    echo "=> [$(date '+%F %T')] $*" | tee -a "$LOGFILE"
 }
 
 # Ensure script runs as root
@@ -26,7 +33,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-PACKAGES=("icinga2" "icinga2-bin" "icinga2-common" "monitoring-plugins")
+
 
 # 1. Stop the service safely
 log "${YELLOW}Stopping Icinga2 service...${NC}"
@@ -52,12 +59,20 @@ remove_pkg () {
 # 3. Force delete leftover directories
 delet_file () {
     log "${YELLOW}Deleting leftover configuration and data directories...${NC}"
-    rm -rf /etc/icinga2
-    log "${GREEN}Removed /etc/icinga2${NC}"
-    rm -rf /var/lib/icinga2
-    log "${GREEN}Removed /var/lib/icinga2${NC}"
-    rm -rf /var/log/icinga2
-    log "${GREEN}Removed /var/log/icinga2${NC}"
+    
+    for FILE in "${ICINGAFILES[@]}"; do
+        if [ -d "$FILE" ]; then
+       log "Folder $FILE exist."
+       log "${GREEN}Removed ${FILE}${NC}"
+       rm -rf $FILE
+    
+    else
+       log "Folder $FILE does not exist, skipping."
+    fi
+    
+    done  
+
+
 }
 
 
